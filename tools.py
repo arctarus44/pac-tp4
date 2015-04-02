8cc041d85babefbde8be76618f97755d652213d3
arctarus44 <arthur.dewarumez@gmail.com>
2015-04-02 17:33:54 +0200

    Ajout d'un fichier contenant des utilitaire.

---
+++ tools.py
@@ -0,0 +1,23 @@
+NAME = "/dewarumez"
+BASE_URL = "http://pac.bouillaguet.info/TP3"
+
+AFF_ERROR = "Error no {0} : {1}"
+
+def print_serverError_exit(err):
+	print(AFF_ERROR.format(err.code, err.msg))
+	exit(1)
+
+def extended_gcd(aa, bb):
+	lastremainder, remainder = abs(aa), abs(bb)
+	x, lastx, y, lasty = 0, 1, 1, 0
+	while remainder:
+		lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
+		x, lastx = lastx - quotient*x, x
+		y, lasty = lasty - quotient*y, y
+	return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
+
+def modinv(a, m):
+	g, x, y = extended_gcd(a, m)
+	if g != 1:
+		raise ValueError
+	return x % m
